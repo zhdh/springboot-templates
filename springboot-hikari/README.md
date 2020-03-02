76234083fc422b5779f34b16fccaa0a1e1ca953e
ghost_home <944369375@qq.com>
2020-03-02 23:15:57 +0800

    新建SpringBoot项目，添加Hikari数据库连接池常用配置

---
+++ springboot-hikari/README.md
@@ -0,0 +1,2 @@
+# 基础项目模板
+> 配置 SpringBoot Hikari 连接池常用参数
---
+++ springboot-hikari/src/test/java/com/example/springboothikari/SpringbootHikariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboothikari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHikariApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
